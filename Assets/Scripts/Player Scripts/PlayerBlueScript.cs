422a65063e5ca5ccf3d9023569ae96922dbfeac8
Aashbir <aashbir@Nikitas-MacBook-Pro.local>
2019-12-27 17:03:02 +0400

    first commit

---
+++ Assets/Scripts/Player Scripts/PlayerBlueScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlueScript : MonoBehaviour
+{
+    private float speed = 5.0f;
+    public VariableJoystick variableJoystick;
+    public Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+    public void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(variableJoystick.Horizontal, variableJoystick.Vertical);
+        rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
+        
+    }
+}
